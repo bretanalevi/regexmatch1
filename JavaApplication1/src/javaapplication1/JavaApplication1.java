e65f08de276679459699e6d4ff8b9f5b47905ee7
Levi Paul Bretaña <bretanalevipaul@gmail.com>
2016-09-15 08:38:40 +0800

    Initial commit

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -1,10 +1,29 @@
-
 package javaapplication1;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class JavaApplication1 {
-
-    public static void main(String[] args) {
-
+   
+    public static boolean isTrue(String str)
+       {
+       return str.matches("[tT]true");
+       }
+    public static boolean containsFalse(String str)
+       {
+       return str.matches(".*false.*");
+       }
+    public static void main(String[] args) throws FileNotFoundException
+    {
+        System.out.println(isTrue("True"));
+        System.out.println(containsFalse("adhfahasdfalsehasdha"));
+        Scanner sc = new Scanner(new FileReader("input.txt"));
+        
+        while(sc.hasNext())
+        {
+            System.out.println(isTrue(sc.nextLine()));
+        }
+        
     }
-    
 }
